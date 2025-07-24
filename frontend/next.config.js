a33b543f0ca4d74628d5c3c6468e0ef3259be428
Trolleroof <nikhilprabhu06@gmail.com>
2025-07-24 20:51:09 +0530

    adding fly changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
   images: { unoptimized: true },
   async rewrites() {
     const useLocalhost = process.env.NEXT_PUBLIC_USE_LOCALHOST === 'true';
-    // const backendUrl = useLocalhost ? 'http://localhost:8000/api' : 'https://cafecode-backend.onrender.com/api';
-    const backendUrl = 'https://v2-bolt-hackathon.onrender.com/api'
+    // const backendUrl = useLocalhost ? 'http://localhost:8000/api' : 'https://cafecode-bacend.fly.dev/api';
+    const backendUrl = 'https://cafecode-bacend.fly.dev/api'
     return [
       {
         source: '/api/:path*',
